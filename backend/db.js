b1b07a951e23f642fe0855e5a148a14f659a180c
Guilherme Burgheri <guilherme.burgheri@hotmail.com>
2026-01-20 11:08:46 -0300

    Atualizando db.js para não ter problemas em dispositivos novos.

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,15 @@
 // backend/server.js
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
+const fs = require("fs");
 
-const dbPath = path.resolve(__dirname, "db", "database.sqlite");
+const dbDir = path.resolve(__dirname, "db");
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir);
+}
+
+const dbPath = path.join(dbDir, "database.sqlite");
+
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) console.error("Erro ao conectar no banco:", err);
   else console.log("Banco conectado com sucesso!");
